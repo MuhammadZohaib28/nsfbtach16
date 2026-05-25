afbbb9a656247a0bb63f977cf147a06a9253c365
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2026-05-25 21:59:57 +0500

    JS day 1

---
+++ day1.js
@@ -0,0 +1,22 @@
+console.log("Mera naam zohaib hai ")
+
+
+console.log(45 + 45)
+
+
+console.log("45 + 45")
+
+
+console.log("Mai Zohaib Hun")
+
+
+console.log("30              + 60")
+
+console.log(30 - "30")
+
+
+console.log("50" / "30")
+
+// DATA TYPES IN JS
+// 1 - STRING
+// 2 - Number
